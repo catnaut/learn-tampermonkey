f665455162a8a9305b8b0a4fbb9a6063ad3d7429
catnaut <catnaut@outlook.com>
2023-12-19 20:39:08 +0800

    helloworld

---
+++ helloworld.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://catnaut.net/
+// @version      2023-12-19
+// @description  hello world
+// @author       catnaut
+// @match        https://blog.catnaut.net
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// @license      GPLv3
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('hello world');
+    // Your code here...
+})();
